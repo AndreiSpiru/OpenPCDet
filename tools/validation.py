839d6d7512015c9ae7510a49e9c0dccd56c53ae7
as5321 <as5321@ic.ac.uk>
2024-04-10 11:15:37 +0100

    Validation works

---
+++ tools/validation.py
@@ -0,0 +1,144 @@
+import argparse
+import glob
+import re
+from pathlib import Path
+import get_bounding_boxes as bbox
+import os
+
+try:
+    import open3d
+    from visual_utils import open3d_vis_utils as V
+    OPEN3D_FLAG = True
+except:
+    import mayavi.mlab as mlab
+    from visual_utils import visualize_utils as V
+    OPEN3D_FLAG = False
+
+import numpy as np
+import torch
+
+from pcdet.config import cfg, cfg_from_yaml_file
+from pcdet.datasets import DatasetTemplate
+from pcdet.models import build_network, load_data_to_gpu
+from pcdet.utils import common_utils
+
+def custom_sort_key(filepath):
+    # Extract file name from file path
+    filename = os.path.basename(filepath)
+    # Extract numeric part using regular expression
+    match = re.match(r'(\d+)_labeled\.bin', filename)
+    if match:
+        return int(match.group(1))
+    else:
+        return filename
+    
+class DemoDataset(DatasetTemplate):
+    def __init__(self, dataset_cfg, class_names, training=True, root_path=None, logger=None, ext='.bin'):
+        """
+        Args:
+            root_path:
+            dataset_cfg:
+            class_names:
+            training:
+            logger:
+        """
+        super().__init__(
+            dataset_cfg=dataset_cfg, class_names=class_names, training=training, root_path=root_path, logger=logger
+        )
+        self.root_path = root_path
+        self.ext = ext
+        data_file_list = glob.glob(str(root_path / f'*{self.ext}')) if self.root_path.is_dir() else [self.root_path]
+        data_file_list = sorted(data_file_list)
+        #print(data_file_list)
+        validation_file_list = glob.glob(str(root_path / f'*verification.npy')) if self.root_path.is_dir() else [self.root_path]
+        validation_file_list = sorted(validation_file_list)
+        #print(validation_file_list)
+        self.validation_file_list = validation_file_list
+        self.sample_file_list = data_file_list
+
+    def __len__(self):
+        return len(self.sample_file_list)
+
+    def __getitem__(self, index):
+        if self.ext == '.bin':
+            points = np.fromfile(self.sample_file_list[index], dtype=np.float32).reshape(-1, 4)
+        elif self.ext == '.npy':
+            points = np.load(self.sample_file_list[index])
+        else:
+            raise NotImplementedError
+
+        input_dict = {
+            'points': points,
+            'frame_id': index,
+        }
+
+        data_dict = self.prepare_data(data_dict=input_dict)
+        return data_dict
+
+
+def parse_config():
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--cfg_file', type=str, default='cfgs/kitti_models/second.yaml',
+                        help='specify the config for demo')
+    parser.add_argument('--data_path', type=str, default='demo_data',
+                        help='specify the point cloud data file or directory')
+    parser.add_argument('--ckpt', type=str, default=None, help='specify the pretrained model')
+    parser.add_argument('--ext', type=str, default='.bin', help='specify the extension of your point cloud data file')
+
+    args = parser.parse_args()
+
+    cfg_from_yaml_file(args.cfg_file, cfg)
+
+    return args, cfg
+
+def main():
+    args, cfg = parse_config()
+    logger = common_utils.create_logger()
+    logger.info('-----------------Quick Demo of OpenPCDet-------------------------')
+    demo_dataset = DemoDataset(
+        dataset_cfg=cfg.DATA_CONFIG, class_names=cfg.CLASS_NAMES, training=False,
+        root_path=Path(args.data_path), ext=args.ext, logger=logger
+    )
+    logger.info(f'Total number of samples: \t{len(demo_dataset)}')
+    validation_file_list = demo_dataset.validation_file_list
+    validation_bboxes = [bbox.get_bounding_box(file) for file in validation_file_list]
+    validation_bboxes = np.stack(validation_bboxes)
+    print(validation_bboxes)
+    model = build_network(model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=demo_dataset)
+    model.load_params_from_file(filename=args.ckpt, logger=logger, to_cpu=True)
+    model.cuda()
+    model.eval()
+    total = 0
+    correct = 0
+    iou = []
+    with torch.no_grad():
+        for idx, data_dict in enumerate(demo_dataset):  
+            total += 1
+            data_dict = demo_dataset.collate_batch([data_dict])
+            #print(data_dict)
+            load_data_to_gpu(data_dict)
+            pred_dicts, _ = model.forward(data_dict)
+            #print(pred_dicts)
+            mask = pred_dicts[0]['pred_labels'] == 1
+            vehicle_bboxes = pred_dicts[0]['pred_boxes'][mask]
+            #print(vehicle_bboxes)
+            true_bbox = validation_bboxes[idx]
+            true_bbox = np.tile(true_bbox, (vehicle_bboxes.shape[0],1))
+            true_bbox = torch.tensor(true_bbox, device ='cuda')
+            max = bbox.bbox3d_overlaps_diou(true_bbox, vehicle_bboxes)
+            print(max)
+            iou.append(max.item())
+            V.draw_scenes(
+                points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
+                ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
+            )
+
+            if not OPEN3D_FLAG:
+                mlab.show(stop=True)
+
+    print(iou)
+    logger.info('Demo done.')
+
+
+if __name__ == '__main__':
+    main()
